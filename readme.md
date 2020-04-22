36484eb6445d8f7fff0f90f1957136681e1ba80d
BinBashBanana <51469593+BinBashBanana@users.noreply.github.com>
2020-04-21 17:34:19 -0700

    001

---
+++ readme.md
@@ -0,0 +1,13 @@
+# HTML Obfuscator
+
+Simple python tool to help you obfuscate your HTML code quickly.
+
+Manual tools also included.
+
+
+
+Usage:
+
+```tool.py [obf|deobf] [filename.html]```
+
+Also note that files deobfuscated with this tool MUST have been obfuscated with it as well.
---
+++ tool.py
@@ -0,0 +1,47 @@
+import sys, os, base64
+from urllib.parse import unquote
+from urllib.parse import quote
+
+usage = '''\nUsage: this.py [obf|deobf] [filename.html]\n\nNOTE: Files deobfuscated with this tool MUST have been obfuscated with it as well.'''
+
+if len(sys.argv)!=3:
+    print(usage)
+    sys.exit(0)
+    sys.exit(0)
+opr=sys.argv[1]
+file=sys.argv[2]
+if not os.path.isfile(file):
+    print ('''\nFile not found!''')
+    sys.exit(0)
+    
+begst = '''<!DOCTYPE html>
+<script type="text/javascript">
+document.write(decodeURIComponent(atob(\''''
+
+endst = '''\')));
+</script>
+<noscript>You must enable javascript in your browser to view this webpage.</noscript>
+'''
+
+
+if opr == "obf":
+    data = open(file, 'r').read()
+    data = quote(data)
+    data = data.encode("utf-8")
+    data = base64.b64encode(data)
+    data = data.decode("utf-8")
+    data = begst + data + endst
+    out = open(file, 'w').write(data)
+    print("\nSuccesfully Obfuscated %s" % file)
+elif opr == "deobf":
+    data = open(file, 'r').read()
+    data = data[88:-102]
+    data = data.encode("utf-8")
+    data = base64.b64decode(data)
+    data = data.decode("utf-8")
+    data = unquote(data)
+    out = open(file, 'w').write(data)
+    print("\nSuccesfully Deobfuscated %s" % file)
+else:
+    print(usage)
+    sys.exit(0)
