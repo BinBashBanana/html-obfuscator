869054c565065d22f7281168eb1a2b8776ce44af
BinBashBanana <51469593+BinBashBanana@users.noreply.github.com>
2021-01-28 15:36:54 -0800

    Very Sturdy Camshaft
    fixed codec problem

--- batch.py
+++ batch.py
@@ -5,8 +5,8 @@ from urllib.parse import quote
 usage = '''\nUsage: this.py [obf|deobf]\nHave a file called list.txt in your working directory,\nwhich lists your HTML files, separated by new lines.\n\nNOTE: Files deobfuscated with this tool MUST have been obfuscated with it as well.'''
 
 if len(sys.argv)!=2:
-    print(usage)
-    sys.exit(0)
+	print(usage)
+	sys.exit(0)
 	
 opr=sys.argv[1]
 lisfile = "list.txt"
@@ -14,8 +14,7 @@ if not os.path.isfile(lisfile):
 	print ('''\nCould not find list.txt!''')
 	sys.exit(0)
 
-rawlis = open(lisfile, 'r').read()
-htlis = rawlis.splitlines()
+htlis = open(lisfile, mode="r", encoding="utf-8").read().splitlines()
 if htlis == []:
 	print ('''\nList.txt is empty!''')
 	sys.exit(0)
@@ -29,35 +28,38 @@ endst = '''\')));
 <noscript>You must enable javascript in your browser to view this webpage.</noscript>
 '''
 
+def dEncode(data):
+	data = quote(data)
+	data = data.encode("utf-8")
+	data = base64.b64encode(data)
+	data = data.decode("utf-8")
+	return data
+
+def dDecode(data):
+	data = data.encode("utf-8")
+	data = base64.b64decode(data)
+	data = data.decode("utf-8")
+	data = unquote(data)
+	return data
 
 if opr == "obf":
 	for file in htlis:
 		if not os.path.isfile(file):
-			print ('''Could not find %s!''' % file)
+			print('''Could not find %s!''' % file)
 			continue
-		data = open(file, 'r').read()
-		data = quote(data)
-		data = data.encode("utf-8")
-		data = base64.b64encode(data)
-		data = data.decode("utf-8")
-		data = begst + data + endst
-		out = open(file, 'w').write(data)
+		data = open(file, mode="r", encoding="utf-8").read()
+		open(file, mode="w", encoding="utf-8").write(begst + dEncode(data) + endst)
 		print("Succesfully Obfuscated %s" % file)
 	
 elif opr == "deobf":
 	for file in htlis:
 		if not os.path.isfile(file):
-			print ('''Could not find %s!''' % file)
+			print('''Could not find %s!''' % file)
 			continue
-		data = open(file, 'r').read()
-		data = data[88:-102]
-		data = data.encode("utf-8")
-		data = base64.b64decode(data)
-		data = data.decode("utf-8")
-		data = unquote(data)
-		out = open(file, 'w').write(data)
+		data = open(file, mode="r", encoding="utf-8").read()
+		open(file, mode="w", encoding="utf-8").write(dDecode(data[88:-102]))
 		print("Succesfully Deobfuscated %s" % file)
 	
 else:
-    print(usage)
-    sys.exit(0)
+	print(usage)
+	sys.exit(0)
--- tool.py
+++ tool.py
@@ -5,14 +5,14 @@ from urllib.parse import quote
 usage = '''\nUsage: this.py [obf|deobf] [filename.html]\n\nNOTE: Files deobfuscated with this tool MUST have been obfuscated with it as well.'''
 
 if len(sys.argv)!=3:
-    print(usage)
-    sys.exit(0)
+	print(usage)
+	sys.exit(0)
 opr=sys.argv[1]
 file=sys.argv[2]
 if not os.path.isfile(file):
-    print ('''\nFile not found!''')
-    sys.exit(0)
-    
+	print('''\nFile not found!''')
+	sys.exit(0)
+	
 begst = '''<!DOCTYPE html>
 <script type="text/javascript">
 document.write(decodeURIComponent(atob(\''''
@@ -22,25 +22,28 @@ endst = '''\')));
 <noscript>You must enable javascript in your browser to view this webpage.</noscript>
 '''
 
+def dEncode(data):
+	data = quote(data)
+	data = data.encode("utf-8")
+	data = base64.b64encode(data)
+	data = data.decode("utf-8")
+	return data
+
+def dDecode(data):
+	data = data.encode("utf-8")
+	data = base64.b64decode(data)
+	data = data.decode("utf-8")
+	data = unquote(data)
+	return data
 
 if opr == "obf":
-    data = open(file, 'r').read()
-    data = quote(data)
-    data = data.encode("utf-8")
-    data = base64.b64encode(data)
-    data = data.decode("utf-8")
-    data = begst + data + endst
-    out = open(file, 'w').write(data)
-    print("\nSuccesfully Obfuscated %s" % file)
+	data = open(file, mode="r", encoding="utf-8").read()
+	open(file, mode="w", encoding="utf-8").write(begst + dEncode(data) + endst)
+	print("\nSuccesfully Obfuscated %s" % file)
 elif opr == "deobf":
-    data = open(file, 'r').read()
-    data = data[88:-102]
-    data = data.encode("utf-8")
-    data = base64.b64decode(data)
-    data = data.decode("utf-8")
-    data = unquote(data)
-    out = open(file, 'w').write(data)
-    print("\nSuccesfully Deobfuscated %s" % file)
+	data = open(file, mode="r", encoding="utf-8").read()
+	open(file, mode="w", encoding="utf-8").write(dDecode(data[88:-102]))
+	print("\nSuccesfully Deobfuscated %s" % file)
 else:
-    print(usage)
-    sys.exit(0)
+	print(usage)
+	sys.exit(0)
